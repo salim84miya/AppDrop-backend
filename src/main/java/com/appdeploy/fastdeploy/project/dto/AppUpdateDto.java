c82f8c950f751ae6d28de3cea759ef6f79fa37ec
salim84miya <salimsaudagar84@gmail.com>
2026-04-27 12:43:26 +0530

    updated app update dto validation

--- src/main/java/com/appdeploy/fastdeploy/project/dto/AppUpdateDto.java
+++ src/main/java/com/appdeploy/fastdeploy/project/dto/AppUpdateDto.java
@@ -1,10 +1,12 @@
 package com.appdeploy.fastdeploy.project.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AppUpdateDto {
 
+    @NotBlank
     private Long id;
 
     private String appName;
